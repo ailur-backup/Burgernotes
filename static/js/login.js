839d04ebc68f2d76aa161ec60559052ce6c99a1e
maaa <no@thankyou.com>
2023-10-13 20:00:34 +0200

    fix dark mode + changes to login page

--- static/js/login.js
+++ static/js/login.js
@@ -13,81 +13,134 @@ let usernameBox = document.getElementById("usernameBox")
 let passwordBox = document.getElementById("passwordBox")
 let statusBox = document.getElementById("statusBox")
 let signupButton = document.getElementById("signupButton")
+let inputNameBox = document.getElementById("inputNameBox")
+let backButton = document.getElementById("backButton")
+
+usernameBox.classList.remove("hidden")
+inputNameBox.innerText = "username:"
+
+let currentInputType = 0
+
+function showInput(inputType) {
+    if (inputType == 0) {
+        usernameBox.classList.remove("hidden")
+        passwordBox.classList.add("hidden")
+        backButton.classList.add("hidden")
+        inputNameBox.innerText = "username:"
+        statusBox.innerText = "log in to your burgernotes account!"
+        currentInputType = 0
+    } else if (inputType == 1) {
+        usernameBox.classList.add("hidden")
+        passwordBox.classList.remove("hidden")
+        backButton.classList.remove("hidden")
+        inputNameBox.innerText = "password:"
+        currentInputType = 1
+    } else if (inputType == 2) {
+        usernameBox.classList.add("hidden")
+        passwordBox.classList.add("hidden")
+        signupButton.classList.add("hidden")
+        backButton.classList.add("hidden")
+        inputNameBox.classList.add("hidden")
+        inputNameBox.innerText = "password:"
+        currentInputType = 2
+    }
+}
 
 function showElements(yesorno) {
     if (!yesorno) {
         usernameBox.classList.add("hidden")
         passwordBox.classList.add("hidden")
         signupButton.classList.add("hidden")
+        backButton.classList.add("hidden")
+        inputNameBox.classList.add("hidden")
+        showInput(currentInputType)
     }
     else {
         usernameBox.classList.remove("hidden")
         passwordBox.classList.remove("hidden")
         signupButton.classList.remove("hidden")
+        backButton.classList.remove("hidden")
+        inputNameBox.classList.remove("hidden")
+        showInput(currentInputType)
     }
 }
 
 signupButton.addEventListener("click", (event) => {
-    async function doStuff() {
-        let username = usernameBox.value
-        let password = passwordBox.value
-
-        if (username == "") {
+    if (passwordBox.classList.contains("hidden")) {
+        if (usernameBox.value == "") {
             statusBox.innerText = "username required"
             return
+        } else {
+            statusBox.innerText = "welcome back, " + usernameBox.value + "!"
         }
-        if (password == "") {
-            statusBox.innerText = "password required"
-            return
-        }
+        showInput(1)
+    } else {
+        async function doStuff() {
+            let username = usernameBox.value
+            let password = passwordBox.value
 
-        showElements(false)
-        statusBox.innerText = "welcome back!"
-
-        async function hashpass(pass) {
-            const key = await hashwasm.argon2id({
-                password: pass,
-                salt: await hashwasm.sha512(pass),
-                parallelism: 1,
-                iterations: 256,
-                memorySize: 512,
-                hashLength: 32,
-                outputType: "encoded"
-            });
-            return key
-        };
-
-        fetch("/api/login", {
-            method: "POST",
-            body: JSON.stringify({
-                username: username,
-                password: await hashpass(password)
-            }),
-            headers: {
-                "Content-type": "application/json; charset=UTF-8"
+            if (password == "") {
+                statusBox.innerText = "password required"
+                return
             }
-        })
-            .then((response) => response)
-            .then((response) => {
-                async function doStuff() {
-                    let responseData = await response.json()
-                    if (response.status == 200) {
-                        localStorage.setItem("DONOTSHARE-secretkey", responseData["key"])
-                        localStorage.setItem("DONOTSHARE-password", await hashwasm.sha512(password))
 
-                        window.location.href = "/app"
-                    }
-                    else if (response.status == 401) {
-                        statusBox.innerText = "wrong username or password :("
-                        showElements(true)
-                    }
-                    else {
-                        statusBox.innerText = "something went wrong! (error code: " + response.status + ")"
-                        showElements(true)
-                    }
+            showInput(2)
+            showElements(true)
+            statusBox.innerText = "signing in.."
+
+            async function hashpass(pass) {
+                const key = await hashwasm.argon2id({
+                    password: pass,
+                    salt: await hashwasm.sha512(pass),
+                    parallelism: 1,
+                    iterations: 256,
+                    memorySize: 512,
+                    hashLength: 32,
+                    outputType: "encoded"
+                });
+                return key
+            };
+
+            fetch("/api/login", {
+                method: "POST",
+                body: JSON.stringify({
+                    username: username,
+                    password: await hashpass(password)
+                }),
+                headers: {
+                    "Content-type": "application/json; charset=UTF-8"
                 }
-                doStuff()
-            });
+            })
+                .then((response) => response)
+                .then((response) => {
+                    async function doStuff() {
+                        let responseData = await response.json()
+                        if (response.status == 200) {
+                            localStorage.setItem("DONOTSHARE-secretkey", responseData["key"])
+                            localStorage.setItem("DONOTSHARE-password", await hashwasm.sha512(password))
+
+                            window.location.href = "/app"
+                        }
+                        else if (response.status == 401) {
+                            statusBox.innerText = "wrong username or password :("
+                            showInput(1)
+                            showElements(true)
+                        }
+                        else {
+                            statusBox.innerText = "something went wrong! (error code: " + response.status + ")"
+                            showInput(1)
+                            showElements(true)
+                        }
+                    }
+                    doStuff()
+                });
+        }
+        doStuff()
     }
-    doStuff()
 });
+
+backButton.addEventListener("click", (event) => {
+    showInput(0)
+});
+
+showInput(0)
