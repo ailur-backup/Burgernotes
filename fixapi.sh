d55d22b76536a55f65d8d252f59ed117a1f9bb08
Tracker-Friendly <arzumify@noreply.centrifuge.hectabit.org>
2024-04-24 11:51:56 +0100

    Delete fixapi.sh

--- fixapi.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Define the old and new URLs
-OLD_URL="https://notes.canary.hectabit.org/api"
-NEW_URL="https://notes.canary.hectabit.org/api"
-
-# Recursively search and replace in files under the current directory
-find . -type f -exec sed -i "s|$OLD_URL|$NEW_URL|g" {} +
