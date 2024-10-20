e8c3b11165e705a4c0570a0f63b4021baee75148
arzumify <jliwin98@danwin1210.de>
2024-10-20 16:53:45 +0100

    Fixed build script

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 path=$(realpath "$(dirname "$0")") || exit 1
 rm -rf "$path/../../services/burgernotes.fgs" || exit 1
 cd "$path" || exit 1
-protoc -I="$path" --go_out="$path" "$path/protobufs/main.proto" || exit 1
+protoc -I="$path" --go_out="$path" "$path/main.proto" || exit 1
 go build -o "$path/../../services/burgernotes.fgs" --buildmode=plugin -ldflags "-s -w" || exit 1
